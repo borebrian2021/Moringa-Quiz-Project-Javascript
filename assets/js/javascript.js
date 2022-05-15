e652ca416f5a6fe0dc162c6d78915be751272cca
borebrian2021 <bkimutai2021@gmail.com>
2022-05-15 08:42:44 +0300

    This commit will hide the questions

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,8 @@ function hideElements(){
   $('#playGame').hide();
   $('#questions').hide();
   $('#final').hide();
+  $('#questions').hide();
+
 
 
 }
@@ -57,27 +59,7 @@ setTimeout(() => {
 
 
 $(document).on('submit', '#my-form2', function() {
-  playerName=$('#name_input').val();
-  $('#name_input').val('');
-  $('#name').text("   | "+playerName);
-  $('#name2').text(playerName);
-
-  setTimeout(() => {
-  $('#welcome').fadeOut('slow');
-
-
-}, "1000");
-setTimeout(() => {
-$('#playGame').fadeIn('slow');
-
-
-}, "2000")
-setTimeout(() => {
-$("#instructions").effect("shake");
-
-}, "4000");
-
-
+  
 return false;
 });
 
